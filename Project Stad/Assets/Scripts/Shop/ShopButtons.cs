f399cb2ce4fc33d0de6980c78ef15cfa21e5eb93
Dominik Beens <dominik.beens@hotmail.nl>
2017-05-10 11:34:25 +0200

    beeje half assed geimplementeerde attribute sprites

--- Project Stad/Assets/Scripts/Shop/ShopButtons.cs
+++ Project Stad/Assets/Scripts/Shop/ShopButtons.cs
@@ -15,33 +15,50 @@ public class ShopButtons : MonoBehaviour
 
     public int buttonIndex;
 
+    public Sprite healthSprite;
+    public Sprite attackDamageSprite;
+    public Sprite attackRangeSprite;
+    public Sprite moveSpeedSprite;
+    public Sprite jumpHeightSprite;
+    public Sprite defenceSprite;
 
     private void Start()
     {
         gm = GameObject.FindWithTag("GM").GetComponent<GameManager>();
         player = GameObject.FindWithTag("Player");
 
+        List<Transform> children = new List<Transform>();
+        foreach (Transform child in gameObject.transform)
+        {
+            children.Add(child);
+        }
+
         switch (buttonIndex)
         {
             case 1:
                 gameObject.GetComponentInChildren<Text>().text = "Max Health Upgrade";
-                //assign sprite
+                children[1].GetComponent<Image>().sprite = healthSprite;
                 //assign cost
                 break;
             case 2:
                 gameObject.GetComponentInChildren<Text>().text = "Attack Damage Upgrade";
+                children[1].GetComponent<Image>().sprite = attackDamageSprite;
                 break;
             case 3:
                 gameObject.GetComponentInChildren<Text>().text = "Attack Range Upgrade";
+                children[1].GetComponent<Image>().sprite = attackRangeSprite;
                 break;
             case 4:
                 gameObject.GetComponentInChildren<Text>().text = "Movement Speed Upgrade";
+                children[1].GetComponent<Image>().sprite = moveSpeedSprite;
                 break;
             case 5:
                 gameObject.GetComponentInChildren<Text>().text = "Jump Height Upgrade";
+                children[1].GetComponent<Image>().sprite = jumpHeightSprite;
                 break;
             case 6:
                 gameObject.GetComponentInChildren<Text>().text = "Defence Upgrade";
+                children[1].GetComponent<Image>().sprite = defenceSprite;
                 break;
         }
     }
